dc88b7791f4f6d02d2cd6b1b2cb4a906433f0eed
Changbeom Choi <me@cbchoi.info>
2020-10-13 23:18:55 +0900

    Agent Intelligence Implemented

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@
 import random
 import math
 import struct
+import numpy as np
 
 class AgentOpt(object):
 	def __init__(self, region, aid, mode):
@@ -18,6 +19,9 @@ class AgentOpt(object):
 	def get_mode(self):
 		return self.mode
 
+	def decrease(self):
+		return -1
+
 class Agent(object):
 	def __init__(self, opt):
 		self.ix = -1
@@ -26,10 +30,15 @@ class Agent(object):
 		self.cy = -1
 		
 		self.opt = opt
+
+		self.schedule_t = 1
 		pass
 
 	def __str__(self):
 		return f"{self.opt.aid}|"
+
+	def get_id(self):
+		return self.opt.aid
 
 	def move(self, perception):
 		next_coord = (0, 0)
@@ -37,6 +46,8 @@ class Agent(object):
 			next_coord = self.random_move()
 		elif self.opt.get_mode() == "OBJ_GREED":
 			next_coord = self.objective_move(perception)
+		elif self.opt.get_mode() == "OBJ_SMART":
+			next_coord = self.smart_move(perception)
 		else:
 			pass
 		
@@ -50,19 +61,11 @@ class Agent(object):
 		self.objective = object_map
 		self.coordX, self.coordY = random.choice(self.objective)
 
+	'''
 	def objective_move(self, perception):
 		if self.objective == None:
 			return self.random_move()
 		else:
-			
-			'''
-			double rad = Math.Atan2((targetPosition.Y - _currentPosition.Y), (targetPosition.X - _currentPosition.X));
-
-            double dx = AgentSpeed * time * Math.Cos(rad);
-            double dy = AgentSpeed * time * Math.Sin(rad);
-            _currentPosition.X += dx;
-            _currentPosition.Y += dy;
-			'''
 			#for cell in perception:
 			perception = sorted(perception, key=lambda cell:cell.get_cell_status())
 			
@@ -73,9 +76,58 @@ class Agent(object):
 			self.ix += dx;
 			self.iy += dy;
 
-#			print((int(self.ix), int(self.iy)), (dx, dy))
-#			print(self.opt.aid, (self.cx - int(self.ix), self.cy - int(self.iy)))
 			return (int(self.ix - self.cx), int(self.iy - self.cy))
+	'''
+
+	def objective_move(self, perception):
+		if self.objective == None:
+			return self.random_move()
+		else:
+			basis = self.cal_weight((self.coordX, self.coordY), (self.ix, self.iy))
+
+			next_move = None
+			for cell in perception:
+				if basis >= self.cal_weight((self.coordX, self.coordY), cell.get_cell_idx()):
+					basis = self.cal_weight((self.coordX, self.coordY), cell.get_cell_idx())
+					next_move = cell.get_cell_idx()
+
+			if next_move:
+				return next_move
+			else:
+				return (0, 0)
+
+	def cal_weight(self, coord1, coord2):
+		return pow((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2, 0.5) 
+
+	def smart_move(self, perception):
+		if self.objective == None:
+			return self.random_move()
+		else:
+			#for cell in perception:
+			# find nearest exit
+			'''
+			basis = self.cal_weight((self.coordX, self.coordY), (self.ix, self.iy))
+			'''
+			exit_coord = self.objective[0]
+			basis = self.cal_weight(self.objective[0], (self.ix, self.iy))
+			
+			for exit in self.objective:
+				if basis >= self.cal_weight(exit, (self.ix, self.iy)):
+					basis = self.cal_weight(exit, (self.ix, self.iy))
+					exit_coord = exit
+					
+			filtered = list(filter(lambda x: len(x.get_agents()) == 0, perception))
+
+			next_move = None
+			for cell in filtered:
+				if basis >= self.cal_weight(exit_coord, cell.get_cell_idx()):
+					basis = self.cal_weight(exit_coord, cell.get_cell_idx())
+					next_move = cell.get_cell_idx()
+
+			if next_move:
+				return next_move
+			else:
+				return (0, 0)
 
 	def register_cell(self, ix, iy):
 		self.ix = ix
@@ -91,3 +143,17 @@ class Agent(object):
 		ba.extend(struct.pack("i", self.opt.aid))
 		
 		return ba
+
+	def penalty(self):
+		self.schedule_t += 5		
+		#print(f"[P]ID:{self.opt.aid}, Time:{self.schedule_t}")
+
+
+	def is_schedule(self):
+		self.schedule_t += self.opt.decrease()
+		#print(f"[S]ID:{self.opt.aid}, Time:{self.schedule_t}")
+		return self.schedule_t <= 0
+
+	def reset_schedule(self):
+		self.schedule_t = 1
+		#print(f"[R]ID:{self.opt.aid}, Time:{self.schedule_t}")
--- region.py
+++ region.py
@@ -3,12 +3,11 @@ import numpy as np
 import random
 
 from adev.cell import Cell
+from adev.cell import UniCell
 
 class Region(object):
-	def __init__(self, region_id, width, height, cell_opt):
+	def __init__(self, region_id, width, height):
 		self.region_id = region_id
-		obj_list = [[Cell(i, j, cell_opt) for i in range(0, width)] for j in range(0, height)]
-		self.region = np.array(obj_list, dtype=object)
 		self.region_agents = []
 		self.active_cells = []
 		self.region_entry_points = {}
@@ -73,6 +72,157 @@ class Region(object):
 
 	def find_exit_port(self, coord):
 		return f"agent_out[{self.region_check_points[coord]}]"
+
+	def schedule(self, _mode = "NORMAL"):
+		if self.active_cells:
+			
+			if _mode == "NORMAL":
+				# random.shuffle(self.active_cells)
+				agents_lst = []
+				for cell in self.active_cells:
+					agents = cell.get_active_agents()
+					agents_lst.extend(agents)
+
+				return agents_lst
+		else:
+			return list()
+
+	def plot(self):
+		print(self.region)
+
+	def region_to_string(self):
+		return self.region.__str__()
+
+	def packing(self):
+		ba = bytearray()
+		ba.append(self.region.shape[0])
+		ba.append(self.region.shape[1])
+
+		for row in self.region:
+			for column in row:
+				ba.append(len(column.get_agents()))
+
+		return ba
+
+class CustomRegion(Region):
+	def __init__(self, region_id, width, height, cell_opt):
+		Region.__init__(self, region_id, width, height)
+		obj_list = [[Cell(i, j, cell_opt) for i in range(0, width)] for j in range(0, height)]
+		self.region = np.array(obj_list, dtype=object)
+		pass
+
+	def __repr__(self):
+		return repr(self.region)
+
+	def alter_cell(self, ix, iy, cell_opt):
+		self.region[iy, ix] = Cell(ix, iy, cell_opt)
+
+import functools
+import operator
+import re
+
+class UniqueRegion(Region):
+	def __init__(self, region_id, width, height):
+		Region.__init__(self, region_id, width, height)
+		obj_list = [[UniCell(i, j) for i in range(0, width)] for j in range(0, height)]
+		self.region = np.array(obj_list, dtype=object)
+		self.waiting_agents = []
+
+
+	def __repr__(self):
+		return repr(self.region)
+
+	def alter_cell(self, ix, iy, cell_opt):
+		self.region[iy, ix] = Cell(ix, iy, cell_opt)
+
+	def add_agent(self, coord, agent):
+		if agent not in self.region_agents: 
+			self.region_agents.append(agent)
+		agent.set_objective(list(self.region_exit_points.values()))
+		self.update_agent(coord, agent)		
+
+	def remove_agent(self, coord, agent):
+		self.region[coord[1], coord[0]].remove_agent(agent)
+
+		if not self.region[coord[1], coord[0]].get_agents():
+			self.active_cells.remove(self.region[coord[1], coord[0]])
+	
+	def update_agent(self, coord, agent):
+		if self.region[coord[1], coord[0]].get_cell_status() == 0:
+			self.region[coord[1], coord[0]].insert_agent(agent)
+
+			if not self.region[coord[1], coord[0]] in self.active_cells:
+				self.active_cells.append(self.region[coord[1], coord[0]])
+		else:
+			agent.penalty()
+
+	def reached_to_exit(self, agent):
+		coord = agent.get_cell_idx()
+		if coord in self.region_check_points.keys():
+			return True
+		else:
+			return False
+
+	def find_exit_port(self, coord):
+		return f"agent_out[{self.region_check_points[coord]}]"
+
+	def pre_execution(self):
+		if self.active_cells:
+			for cell in self.active_cells:
+				agents = cell.get_active_agents()
+				self.waiting_agents.extend(agents)
+				
+			random.shuffle(self.waiting_agents)
+			return self.waiting_agents
+		else:
+			return None
+	
+	def execution(self):
+		candidate_move = {}
+
+		if self.waiting_agents:
+			for agent in self.waiting_agents:
+			# TODO call agent's perceive function
+				perception = []
+				ix, iy = agent.get_cell_idx()
+
+				if ix > 0 and ix < self.region.shape[1]-1 and \
+				iy > 0 and iy < self.region.shape[0]-1 :                   
+					perception = functools.reduce(operator.iconcat, self.region[iy-1:iy+2, ix-1:ix+2].tolist(), [])
+				elif ix == 0 and iy > 0 and iy < self.region.shape[0]-1 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy-1:iy+2, ix:ix+2].tolist(), [])
+				elif iy == 0 and ix > 0 and ix < self.region.shape[1]-1 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy:3, ix-1:ix+1].tolist(), [])
+				elif iy == 0 and ix == 0 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy:iy+2, ix:ix+2].tolist(), [])
+				elif ix == self.region.shape[1]-1 and iy > 0 and iy < self.region.shape[0]-1 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy-1:iy+1, ix-1:ix+1].tolist(), [])
+				elif iy == self.region.shape[0]-1 and ix > 0 and ix < self.region.shape[1]-1 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy-1:iy+1, ix-1:ix+1].tolist(), [])
+				elif iy == self.region.shape[0]-1 and ix == self.region.shape[1]-1 : 
+					perception = functools.reduce(operator.iconcat, self.region[iy-1:iy+1, ix-1:ix+1].tolist(), [])
+
+				nx, ny = agent.move(perception)
+				if not(nx == 0 and ny == 0): 
+					if self.region[ny, nx].get_agents():
+						agent.penalty()
+					else:
+						self.remove_agent((ix, iy), agent)
+						self.update_agent((nx, ny), agent)
+						agent.reset_schedule()
+
+			self.waiting_agents.clear()
+		pass
+	
+	def post_execution(self):
+		exit_agent_lst = []
+		for coord in self.region_check_points.keys():
+			if self.region[coord[1], coord[0]].get_agents():
+				exit_agent_lst.extend(self.region[coord[1], coord[0]].get_agents())
+				for agent in self.region[coord[1], coord[0]].get_agents():
+					self.remove_agent(coord, agent)
+					self.region_agents.remove(agent)
+		return exit_agent_lst
 
 	def schedule(self, _mode = "NORMAL"):
 		if self.active_cells:
